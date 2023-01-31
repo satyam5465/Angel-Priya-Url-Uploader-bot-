4f70d5ad1db8f5ce169d81ef352d5871b492c928
Ezee19 <sonukumarkrbbu67@gmail.com>
2023-02-01 02:55:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,4 +29,4 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     #Admin id is stored in 
-    LAZY_DEVELOPER = set(int(x) for x in os.environ.get("LAZY_ADMIN", "").split())
+    LAZY_DEVELOPER = set(int(x) for x in os.environ.get("LAZY_ADMIN", "5791145987").split())
